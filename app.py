330125a3182b6fa62fea774d35220f67b54ca77b
kryukovaeks <47472303+kryukovaeks@users.noreply.github.com>
2023-06-20 21:25:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from dotenv import load_dotenv
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
